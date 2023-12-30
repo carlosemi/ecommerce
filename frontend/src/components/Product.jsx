e6db979e8eb86c9b6aef5f5a67f920fdd5d0754c
carlosemi <carlos-egf@hotmail.com>
2023-12-30 11:17:58 -0600

    Added product rating icons (stars) and num reviews

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,5 +1,6 @@
 import {Card} from 'react-bootstrap';
 import {Link} from 'react-router-dom';
+import Rating from './Rating';
 
 const Product = ({product}) => {
   return (
@@ -16,10 +17,9 @@ const Product = ({product}) => {
             </Link>
 
             <Card.Text as='div'>
-                <div className='my-3'>
-                    {product.rating} from {product.numReviews} reviews
-                </div>
+                <Rating value={product.rating} text={`${product.numReviews} reviews`}/>
             </Card.Text>
+
 
             <Card.Text as='h3'>
                 ${product.price}
