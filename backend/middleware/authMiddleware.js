b06445052e21734b6c856fd5018a76746c895a10
carlosemi <carlos-egf@hotmail.com>
2024-01-03 15:28:04 -0600

    Added user profile endpoint functionality

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -19,7 +19,7 @@ const protect = asyncHandler(async (req, res, next) => {
     // Verify token
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = await User.findById(decoded.userID).select('-password');
+        req.user = await User.findById(decoded.userId).select('-password');
         next();
     } catch (error) {
         console.log(error);
